1ea92483e93c1a8d575dcb21f2674c5d948882a8
AfifaSiddiquee <afeefaasiddiqueee98@gmail.com>
2026-02-05 16:31:50 +0500

    Update feature_pipeline.py

--- pipelines/feature_pipeline.py
+++ pipelines/feature_pipeline.py
@@ -1,10 +1,13 @@
 import os
 import pandas as pd
+import hopsworks
 from src.ingestion.fetch_aqi import fetch_aqi
 
 def run_feature_pipeline():
     record = fetch_aqi()
     df = pd.DataFrame([record])
+
+    df["timestamp"] = pd.to_datetime(df["timestamp"])
 
     df["hour"] = df["timestamp"].dt.hour
     df["day"] = df["timestamp"].dt.day
@@ -14,8 +17,21 @@ def run_feature_pipeline():
     pollutant_cols = ["pm25", "pm10", "co", "no2", "so2", "o3"]
     df[pollutant_cols] = df[pollutant_cols].fillna(-1)
 
-    os.makedirs("data/processed", exist_ok=True)
-    df.to_csv("data/processed/aqi_features.csv", index=False)
+    # 🔹 Connect to Hopsworks
+    project = hopsworks.login(api_key_value=os.getenv("HOPSWORKS_API_KEY"))
+    fs = project.get_feature_store()
+
+    fg = fs.get_or_create_feature_group(
+        name="aqi_features",
+        version=1,
+        primary_key=["city"],
+        event_time="timestamp",
+        description="AQI features from OpenWeather"
+    )
+
+    fg.insert(df)
+
+    print("Feature pipeline completed & data inserted into Hopsworks")
 
 if __name__ == "__main__":
     run_feature_pipeline()
